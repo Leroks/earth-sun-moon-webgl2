582b9f6432b44ce0810bb5efa4da1925401cfbe3
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-12-03 06:11:30 +0300

    Earth rotating around the sun.

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl2');
 
-const outerRadius = 0.5;
-const innerRadius = 0.2;
+// Set canvas background color to black
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-const vertices = [];
-// Center of the star
-vertices.push(0.0, 0.0);
+// Adjust viewport and aspect ratio
+gl.viewport(0, 0, canvas.width, canvas.height);
+const aspectRatio = canvas.width / canvas.height;
+
+const outerRadiusSun = 0.5;
+const innerRadiusSun = 0.2;
+
+const verticesSun = [];
+// Center of the sun
+verticesSun.push(0.0, 0.0);
 
 for (let i = 0; i <= 20; i++) {
-    const radius = i % 2 === 0 ? outerRadius : innerRadius;
+    const radius = i % 2 === 0 ? outerRadiusSun : innerRadiusSun;
     const theta = (i / 10) * Math.PI; // Angle for each point
 
     const x = radius * Math.cos(theta);
     const y = radius * Math.sin(theta);
 
-    vertices.push(x, y);
+    verticesSun.push(x, y);
+}
+
+const outerRadiusEarth = 0.3;
+const innerRadiusEarth = 0.1;
+
+const distanceFromSun = 1.0; // Adjust the distance of Earth from the Sun
+
+const verticesEarth = [];
+// Center of the Earth (position it away from the Sun)
+verticesEarth.push(distanceFromSun, 0.0);
+
+for (let i = 0; i <= 20; i++) {
+    const radius = i % 2 === 0 ? outerRadiusEarth : innerRadiusEarth;
+    const theta = (i / 10) * Math.PI; // Angle for each point
+
+    const x = distanceFromSun + radius * Math.cos(theta);
+    const y = radius * Math.sin(theta);
+
+    verticesEarth.push(x, y);
 }
 
 const vertexShaderSource = `
   attribute vec2 aPosition;
   uniform float uRotation; // Rotation angle in radians
+  uniform float uDistance; // Distance from the center
   void main() {
     // Create a rotation matrix
     mat2 rotationMatrix = mat2(cos(uRotation), -sin(uRotation), sin(uRotation), cos(uRotation));
@@ -28,19 +55,32 @@ const vertexShaderSource = `
     // Apply rotation to the position
     vec2 rotatedPosition = rotationMatrix * aPosition;
 
-    gl_Position = vec4(rotatedPosition, 0.0, 1.0);
+    // Translate the position based on the distance
+    vec2 translatedPosition = rotatedPosition + vec2(uDistance, 0.0);
+
+    gl_Position = vec4(translatedPosition, 0.0, 1.0);
   }
 `;
 
 const fragmentShaderSource = `
   void main() {
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); // Yellow color
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); // Yellow color for the Sun
   }
 `;
 
-const vertexBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+const fragmentShaderSourceEarth = `
+  void main() {
+    gl_FragColor = vec4(0.0, 0.7, 1.0, 1.0); // Light blue color for the Earth
+  }
+`;
+
+const vertexBufferSun = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferSun);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesSun), gl.STATIC_DRAW);
+
+const vertexBufferEarth = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferEarth);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesEarth), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vertexShaderSource);
@@ -50,33 +90,49 @@ const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, fragmentShaderSource);
 gl.compileShader(fragmentShader);
 
-const shaderProgram = gl.createProgram();
-gl.attachShader(shaderProgram, vertexShader);
-gl.attachShader(shaderProgram, fragmentShader);
-gl.linkProgram(shaderProgram);
+const fragmentShaderEarth = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShaderEarth, fragmentShaderSourceEarth);
+gl.compileShader(fragmentShaderEarth);
 
-gl.useProgram(shaderProgram);
+const shaderProgramSun = gl.createProgram();
+gl.attachShader(shaderProgramSun, vertexShader);
+gl.attachShader(shaderProgramSun, fragmentShader);
+gl.linkProgram(shaderProgramSun);
 
-const aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
-gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(aPosition);
-
-const uRotation = gl.getUniformLocation(shaderProgram, 'uRotation');
-
-gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+const shaderProgramEarth = gl.createProgram();
+gl.attachShader(shaderProgramEarth, vertexShader);
+gl.attachShader(shaderProgramEarth, fragmentShaderEarth);
+gl.linkProgram(shaderProgramEarth);
 
 function draw() {
-    // Rotate 0.01 radians per frame
-    const rotationAngle = performance.now() * 0.001;
-
-    // Set the rotation angle in the shader
-    gl.uniform1f(uRotation, rotationAngle);
-
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 2);
+
+    // Rotation for the Sun
+    const rotationAngleSun = performance.now() * 0.001;
+    drawStar(shaderProgramSun, vertexBufferSun, rotationAngleSun);
+
+    // Rotation for the Earth (rotate around the Sun)
+    const rotationAngleEarth = performance.now() * 0.0005;
+    drawStar(shaderProgramEarth, vertexBufferEarth, rotationAngleEarth, 0.0);
 
     requestAnimationFrame(draw);
+}
+
+function drawStar(program, buffer, rotationAngle, distance = 0.0) {
+    gl.useProgram(program);
+
+    const aPosition = gl.getAttribLocation(program, 'aPosition');
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
+    const uRotation = gl.getUniformLocation(program, 'uRotation');
+    gl.uniform1f(uRotation, rotationAngle);
+
+    const uDistance = gl.getUniformLocation(program, 'uDistance');
+    gl.uniform1f(uDistance, distance);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesSun.length / 2);
 }
 
 draw();
