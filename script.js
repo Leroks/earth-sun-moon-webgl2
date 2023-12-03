2afc0736e828a617bd73466e52a0dea7be86619e
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-12-03 05:50:40 +0300

    Rotating star.

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl2');
-
 
 const outerRadius = 0.5;
 const innerRadius = 0.2;
 
 const vertices = [];
-
 // Center of the star
 vertices.push(0.0, 0.0);
 
@@ -21,34 +18,38 @@ for (let i = 0; i <= 20; i++) {
     vertices.push(x, y);
 }
 
-
-
 const vertexShaderSource = `
   attribute vec2 aPosition;
+  uniform float uRotation; // Rotation angle in radians
   void main() {
-    gl_Position = vec4(aPosition, 0.0, 1.0);
+    // Create a rotation matrix
+    mat2 rotationMatrix = mat2(cos(uRotation), -sin(uRotation), sin(uRotation), cos(uRotation));
+    
+    // Apply rotation to the position
+    vec2 rotatedPosition = rotationMatrix * aPosition;
+
+    gl_Position = vec4(rotatedPosition, 0.0, 1.0);
   }
 `;
+
 const fragmentShaderSource = `
   void main() {
     gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); // Yellow color
   }
 `;
+
 const vertexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-// Compile the vertex shader
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vertexShaderSource);
 gl.compileShader(vertexShader);
 
-// Compile the fragment shader
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, fragmentShaderSource);
 gl.compileShader(fragmentShader);
 
-// Link the shaders to form a program
 const shaderProgram = gl.createProgram();
 gl.attachShader(shaderProgram, vertexShader);
 gl.attachShader(shaderProgram, fragmentShader);
@@ -56,11 +57,26 @@ gl.linkProgram(shaderProgram);
 
 gl.useProgram(shaderProgram);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 const aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
 gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(aPosition);
 
-gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear the canvas with black color
+const uRotation = gl.getUniformLocation(shaderProgram, 'uRotation');
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
-gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 2);
+
+function draw() {
+    // Rotate 0.01 radians per frame
+    const rotationAngle = performance.now() * 0.001;
+
+    // Set the rotation angle in the shader
+    gl.uniform1f(uRotation, rotationAngle);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 2);
+
+    requestAnimationFrame(draw);
+}
+
+draw();
